84d4ca573f0bb956cfa16c8fa1c1db8a80b898e5
Silvia <91378451+NEW-NERO@users.noreply.github.com>
2024-06-21 19:58:16 -0400

    Update sticker.js

--- src/commands/sticker.js
+++ src/commands/sticker.js
@@ -3,18 +3,18 @@ export default [{
   command: ["sticker","stiker","s","wm","stickerwm","stikerwm","swm"],
   code: async({ client, m, quoted, mimetype, text }) => {
     try {
-      if(!/image|video|webp/i.test(mimetype)) return m.reply("Balas gambar atau video yang ingin dijadikan sticker");
+      if(!/image|video|webp/i.test(mimetype)) return m.reply("Responde a la imagen o vídeo que quieres hacer un stickers");
       const buffer = await quoted.download();
       let exif;
       if(text) {
         const [name, author] = text.split("|");
         exif = { packName: name ? name : "", packPublish: author ? author : "" };
       } else {
-        exif = { packName: "Created by", packPublish: "@ Amira-MD" };
+        exif = { packName: "Created by", packPublish: "@ Shizu-MD" };
       }
       m.reply(buffer, { asSticker: true, ...exif });
     } catch (e) {
-      m.reply("Terjadi kesalahan pada command!");
+      m.reply("Se produjo un error en el comando.!");
       console.log(e)
     }
   }
